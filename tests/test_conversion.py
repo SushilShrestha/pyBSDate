3ac829ef3a7d580f47d8d786be6fb411ff89004a
Sushil Shrestha <afahocci@gmail.com>
2015-06-02 16:21:26 +0545

    Refactoring

--- tests/test_conversion.py
+++ tests/test_conversion.py
@@ -1,34 +1,35 @@
 __author__ = 'sushil'
 import unittest
 from pyBSDate import utilities
+from pyBSDate import BSDate
 from pyBSDate import exceptions
 
 class TestDateConversions_BS2AD(unittest.TestCase):
     def test_wrong_formatting(self):
         wrong_formatted_date = "01-10-2072"
         with self.assertRaises(exceptions.InvalidDateFormat):
-            utilities.convert_to_ad(wrong_formatted_date)
+            BSDate.convert_to_ad(wrong_formatted_date)
 
     def test_over_the_range_dates(self):
         over_range_date = "1900-01-10"
         with self.assertRaises(exceptions.DateOutOfRange):
-            utilities.convert_to_ad(over_range_date)
+            BSDate.convert_to_ad(over_range_date)
 
     def test_invalid_month(self):
         invalid_month = "2072-18-10"
         with self.assertRaises(exceptions.InvalidDate):
-            utilities.convert_to_ad(invalid_month)
+            BSDate.convert_to_ad(invalid_month)
 
     def test_invalid_day(self):
         invalid_day = "2072-01-33"
         with self.assertRaises(exceptions.InvalidDate):
-            utilities.convert_to_ad(invalid_day)
+            BSDate.convert_to_ad(invalid_day)
 
     def test_invalid_date(self):
         # no 32 in ashoj 2072
         invalid_date = "2072-06-32"
         with self.assertRaises(exceptions.InvalidDate):
-            utilities.convert_to_ad(invalid_date)
+            BSDate.convert_to_ad(invalid_date)
 
     def test_date_to_days(self):
         year, month, day = 2072, 04, 10
@@ -41,14 +42,14 @@ class TestDateConversions_BS2AD(unittest.TestCase):
         bs_date = "2072-01-10"
         ad_date = "2015-04-23"
 
-        converted_date = utilities.convert_to_ad(bs_date)
+        converted_date = BSDate.convert_to_ad(bs_date)
         self.assertEqual(converted_date, ad_date)
 
     def test_leap_year_test(self):
         bs_date = "2072-11-19"
         ad_date = "2016-03-02"
 
-        converted_date = utilities.convert_to_ad(bs_date)
+        converted_date = BSDate.convert_to_ad(bs_date)
         self.assertEqual(converted_date, ad_date)
 
 
@@ -57,33 +58,34 @@ class TestDateConversions_AD2BS(unittest.TestCase):
     def test_wrong_formatting(self):
         wrong_formatted_date = "01-10-2072"
         with self.assertRaises(exceptions.InvalidDateFormat):
-            utilities.convert_to_bs(wrong_formatted_date)
+            BSDate.convert_to_bs(wrong_formatted_date)
 
     def test_over_the_range_dates(self):
-        over_range_date = "1900-01-10"
+        over_range_date = "1913-12-29"
+        over_range_date = "2044-01-13"
         with self.assertRaises(exceptions.DateOutOfRange):
-            utilities.convert_to_bs(over_range_date)
+            BSDate.convert_to_bs(over_range_date)
 
     def test_invalid_month(self):
-        invalid_month = "2072-18-10"
+        invalid_month = "2015-18-10"
         with self.assertRaises(exceptions.InvalidDate):
-            utilities.convert_to_bs(invalid_month)
+            BSDate.convert_to_bs(invalid_month)
 
     def test_invalid_day(self):
-        invalid_day = "2072-01-33"
+        invalid_day = "2015-01-33"
         with self.assertRaises(exceptions.InvalidDate):
-            utilities.convert_to_bs(invalid_day)
+            BSDate.convert_to_bs(invalid_day)
 
     def test_invalid_date(self):
         invalid_date = "2016-02-31"
         with self.assertRaises(exceptions.InvalidDate):
-            utilities.convert_to_bs(invalid_date)
+            BSDate.convert_to_bs(invalid_date)
 
     def test_ad_to_bs(self):
         bs_date = "2072-01-10"
         ad_date = "2015-04-23"
 
-        converted_date = utilities.convert_to_bs(ad_date)
+        converted_date = BSDate.convert_to_bs(ad_date)
         self.assertEqual(converted_date, bs_date)
 
         # print _ad_to_bs(2015, 06, 2), "2072-02-19"
--- utilities.py
+++ utilities.py
@@ -1,23 +1,6 @@
 __author__ = 'sushil'
 import re
 from exceptions import InvalidDateFormat, DateOutOfRange, InvalidDate
-def convert_to_ad(bs_date):
-    date_components = decompose_date(bs_date)
-    year, month, day = date_components
-    validate_date_bs(year, month, day)
-
-    ad_year, ad_month, ad_day = _bs_to_ad(year, month, day)
-    formatted_date = "{}-{:02}-{:02}".format(ad_year, ad_month, ad_day)
-    return formatted_date
-
-def convert_to_bs(ad_date):
-    date_components = decompose_date(ad_date)
-    year, month, day = date_components
-    validate_date_ad(year, month, day)
-
-    bs_year, bs_month, bs_day = _ad_to_bs(year, month, day)
-    formatted_date = "{}-{:02}-{:02}".format(bs_year, bs_month, bs_day)
-    return formatted_date
 
 import json
 DATE_MAP = json.load(open("../datemap.json"))
@@ -25,15 +8,44 @@ AD_MONTH_DAYS = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 AD_MONTH_DAYS_LEAP = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def validate_date_bs(year, month, day):
+    '''
+    Answers BS date is valid or not. Raise InvalidDate exception if date is invalid.
+    :param year:
+    :param month:
+    :param day:
+    :return: True if valid
+    '''
+    # We have mapping for the year
     year_calendar = DATE_MAP.get(str(year))
     if not year_calendar:
-        return
+        raise DateOutOfRange
+
+    if month>12 or day>32:
+        raise InvalidDate
+
+    # Month have valid days
     days_in_month = year_calendar['daysonmonth']
     day_in_month = days_in_month[month-1]
     if day>day_in_month:
         raise InvalidDate
 
+    return True
+
 def validate_date_ad(year, month, day):
+    '''
+    Answers AD date is valid or not. Raise InvalidDate exception if date is invalid.
+    :param year:
+    :param month:
+    :param day:
+    :return:
+    '''
+    # chop off tails we don't want our code to break and too lazy to chop off precise
+    validate_date_bs(year+56, 1, 1)
+    validate_date_bs(year+57, 1, 1)
+
+    if month>12 or day>31:
+        raise InvalidDate
+
     days_in_month = AD_MONTH_DAYS
     if is_leap_year(year):
         days_in_month = AD_MONTH_DAYS_LEAP
@@ -42,9 +54,14 @@ def validate_date_ad(year, month, day):
         raise InvalidDate
 
 def _bs_to_ad(year, month, day):
+    '''
+    Convert BS to AD
+    :param year: int
+    :param month: int
+    :param day: int
+    :return: AD date for valid BS
+    '''
     conversion_information = DATE_MAP.get(str(year))
-    if not conversion_information:
-        raise DateOutOfRange
 
     first_day_on_year = conversion_information['1stbaisakh']
     ad_year, ad_month, ad_day = decompose_date(first_day_on_year)
@@ -69,13 +86,19 @@ def _bs_to_ad(year, month, day):
     return ad_year, ad_month, ad_day
 
 def _ad_to_bs(year, month, day):
+    '''
+    Convert AD to BS
+    :param year: int
+    :param month: int
+    :param day: int
+    :return: BS date for valid AD
+    '''
+    # get rough estimates
     bs_year = year + 57
     bs_day = 1
     bs_month = 1
 
     conversion_information = DATE_MAP.get(str(bs_year))
-    if not conversion_information:
-        raise DateOutOfRange
 
     first_day_on_year = conversion_information['1stbaisakh']
     ad_year, ad_month, ad_day = decompose_date(first_day_on_year)
@@ -102,6 +125,7 @@ def _ad_to_bs(year, month, day):
             day_in_month = days_in_month[bs_month-1]
         return bs_year, bs_month, bs_day
 
+    # if days to add is negative we have to backtrace
     # backward from <==april
     bs_day += total_num_days_to_add
 
@@ -116,9 +140,14 @@ def _ad_to_bs(year, month, day):
         bs_day += days_in_previous_month
     return bs_year, bs_month, bs_day
 
-    pass
-
 def _convert_to_days(month, day, days_on_month):
+    '''
+    Gives number of days from 1st date of the year
+    :param month:
+    :param day:
+    :param days_on_month:
+    :return:
+    '''
     total_days = 0
     for i in days_on_month[:month-1]:
         total_days += i
@@ -126,27 +155,27 @@ def _convert_to_days(month, day, days_on_month):
     return total_days
 
 def decompose_date(date):
+    '''
+    Gives tuple of year, month, day if format is valid else raise InvalidFormatException
+    :param date:
+    :return:
+    '''
     re_date_format = r'(\d{4})[\-\/](\d{1,2})[\-\/](\d{1,2})'
 
     valid_date = re.match(re_date_format, date)
     if not valid_date:
         raise InvalidDateFormat
 
-    year, month, day = valid_date.groups()
-
-    year = int(year)
-    month = int(month)
-    day = int(day)
-
-    if month<1 or month>12:
-        raise InvalidDate
-
-    if day<1 or day>32:
-        raise InvalidDate
+    year, month, day = map(int, valid_date.groups())
 
     return year,month,day
 
 def is_leap_year(year):
+    '''
+    For AD
+    :param year:
+    :return: boolean
+    '''
     if year%4 == 0:
         if year%100 == 0:
             if year%400 ==0:
