7bd5f8c2c797a577a45255f22d9873b1546c6200
Sushil Shrestha <afahocci@gmail.com>
2020-06-15 00:28:33 -1000

    update setup.py?

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
-from distutils.core import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='pyBSDate',
-    version='0.2.2',
+    version='0.3.0rc',
     packages=['pyBSDate'],
     url='https://github.com/SushilShrestha/pyBSDate',
-    download_url='https://github.com/SushilShrestha/pyBSDate/tarball/0.2.2',
-    license='',
+    download_url='https://github.com/SushilShrestha/pyBSDate/tarball/0.3.0rc',
+    license='MIT',
     author='sushil',
     author_email='afahocci@gmail.com',
     package_data={},
-    description='Python BS date conversion utility'
+    description='Python BS date conversion utility',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
